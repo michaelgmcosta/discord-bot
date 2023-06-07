1fd4a5d62acf67a14aef48ce9e31d867a352def1
Michael Costa <michaelgm.costa@gmail.com>
2023-06-07 20:57:16 -0300

    Update IA model

--- src/discord_api/discord_client.py
+++ src/discord_api/discord_client.py
@@ -8,23 +8,29 @@ from openai_api.openapi_connector import get_chat_gpt_response
 
 load_dotenv()
 
-bot_caller: str = "!"
+bot_caller: str = "/bot"
 
 discord_token = os.environ.get("DISCORD_TOKEN")
 
 
 class CustomDiscordClient(Client):
     async def on_ready(self):
-        print(f"Estou conectado com o user {self.user}")
+        print(f"--- Estou conectado ao Discord com o user {self.user} ---")
 
     async def on_message(self, message: Message):
         message_content: str = message.content
         if bot_caller in message_content:
             prompt: str = message_content.split(bot_caller)[1]
-            print(f"------------- Prompt message: {prompt} -------------")
+            print(f"--- User message: {prompt} ---")
+            await message.channel.send(content="Processando resposta...")
             chat_gpt_response: str = get_chat_gpt_response(question=prompt)
             if chat_gpt_response:
+                print(f"--- IA message: {chat_gpt_response} ---")
                 await message.channel.send(content=chat_gpt_response)
+            else:
+                await message.channel.send(
+                    content="Erro ao processar pergunta, tente novamente."
+                )
 
 
 intents = Intents.default()
