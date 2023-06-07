af5c3e6e73ad57e735d7660e89a54eaf7df1c702
Michael Costa <michaelgm.costa@gmail.com>
2023-06-07 20:58:06 -0300

    Update IA model

--- src/openai_api/openapi_connector.py
+++ src/openai_api/openapi_connector.py
@@ -9,13 +9,14 @@ openai.api_key = os.environ.get("CHAT_GTP_API_KEY")
 
 
 def get_chat_gpt_response(question: str) -> str | None:
-    response_from_chat_gpt = openai.Completion.create(
-        model="text-davinci-003", prompt=question, max_tokens=200
+    response_from_chat_gpt = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", messages=[{"role": "user", "content": question}]
     )
 
     if response_from_chat_gpt:
         choices: List[Dict[str, Any]] = response_from_chat_gpt.get("choices", None)
         if choices and len(choices) > 0:
-            text: str = choices[0].get("text", None)
+            message = choices[0].get("message", None)
+            text: str = message.get("content", None)
             return text
     return None
